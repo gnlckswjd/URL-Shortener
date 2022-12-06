ca2f146a659572fdef0e50c6bdf15bd3bcdbc687
Oak_Cassia <gnlckswjd1@gmail.com>
2022-12-06 22:51:32 +0900

    Init

---
+++ URLShortener/URLShortener/URLShortener.cpp
@@ -0,0 +1,14 @@
+﻿#include  "pch.h"
+
+#include <iostream>
+
+#include "WinSock2.h"
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
