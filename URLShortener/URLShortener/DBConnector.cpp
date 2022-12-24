9c8a903662f86aa37867da16e3b8979eb28f2cea
Oak_Cassia <gnlckswjd1@gmail.com>
2022-12-24 23:53:52 +0900

    add mutex lock in DBConnector

--- URLShortener/URLShortener/DBConnector.cpp
+++ URLShortener/URLShortener/DBConnector.cpp
@@ -5,60 +5,9 @@
 #include <mysql.h>
 #pragma  comment(lib, "libmySQL.lib")
 
-
 atomic<unsigned __int64> DBConnector::shortIndex = 0;
+mutex DBConnector::dbMutex;
 
-void Test()
-{
-
-
-    printf("%s\n", mysql_get_client_info());
-
-    MYSQL Conn;                 // MySQL 정보를 담을 구조체
-    MYSQL* ConnPtr = NULL;      // MYSQL 핸들
-    MYSQL_RES* Result;          // 쿼리성공시 결과를 담는 구조체 포인터
-    MYSQL_ROW ROW;              // 쿼리성공시 결과로 나온 행의 정보를 담는 구조체
-    int Stat;                   // 쿼리요청 후 결과(성공, 실패)
-
-    mysql_init(&Conn); //MySQL 정보 초기화
-
-    //데이터베이스와 연결
-    ConnPtr = mysql_real_connect(&Conn, "localhost", "root", "1234", "mydb", 3306, NULL, 0);
-
-    if (ConnPtr == NULL)
-    {
-        cout << "Connection Error" << endl;
-        return;
-    }
-
-    const char* Query = "SELECT * FROM urlinfo";
-    Stat = mysql_query(ConnPtr, Query);
-    if (Stat != 0)
-    {
-        cout << "Query Error" << endl;
-    }
-    Result = mysql_store_result(ConnPtr);
-
-    while ((ROW = mysql_fetch_row(Result)) != NULL)
-        cout << ROW[0] << " " << ROW[1] << endl;
-    mysql_free_result(Result);
-
-
-    Query = "SELECT * FROM urlinfo";
-    Stat = mysql_query(ConnPtr, Query);
-    if (Stat != 0)
-    {
-        cout << "Query Error" << endl;
-    }
-    Result = mysql_store_result(ConnPtr);
-
-    while ((ROW = mysql_fetch_row(Result)) != NULL)
-        cout << ROW[0] << " " << ROW[1] << endl;
-    mysql_free_result(Result);
-
-
-    mysql_close(ConnPtr);
-}
 
 DBConnector::DBConnector()
 {
@@ -73,79 +22,79 @@ DBConnector::~DBConnector()
         delete Conn;
 }
 
-
-void DBConnector::Disconnect(MYSQL* ConnPtr)
+void DBConnector::DisconnectDB(MYSQL* ConnPtr)
 {
     if (ConnPtr != nullptr)
         mysql_close(ConnPtr);
     ConnPtr = nullptr;
 }
 
-
-
 bool DBConnector::SearchLongURL_Query(string url, string& shortURL)
 {
     MYSQL* ConnPtr = nullptr;
     MYSQL_RES* Result;         
     MYSQL_ROW ROW;             
-    int Stat;                  
-    
-    ConnPtr = mysql_real_connect(Conn, "localhost", "root", "1234", "mydb", 3306, NULL, 0);
+    int Stat;
 
-    if (ConnPtr == nullptr)
     {
-        return false;
+        lock_guard<mutex> guard(dbMutex);
+
+        ConnPtr = mysql_real_connect(Conn, "localhost", "root", "1234", "mydb", 3306, NULL, 0);
+        if (ConnPtr == nullptr)
+        {
+            return false;
+        }
+
+        unsigned long long index;
+        GetShortURL_Index(index);
+
+        string query = format("INSERT INTO url_info (short_url,long_url)"
+            "SELECT {0}, '{1}' FROM dual WHERE NOT EXISTS"
+            "(SELECT * FROM url_info WHERE `long_url` = '{1}');", index, url);
+
+        Stat = mysql_query(ConnPtr, query.c_str());
+        if (Stat != 0)
+        {
+            cout << "Query Error" << mysql_error(Conn) << endl;
+
+            DisconnectDB(ConnPtr);
+            return false;
+        }
+
+        string query2 = format("SELECT short_url FROM url_info where long_url = '{}';", url);
+        Stat = mysql_query(ConnPtr, query2.c_str());
+        if (Stat != 0)
+        {
+            cout << "Query Error" << mysql_error(Conn) << endl;
+            //TODO: 처리
+            DisconnectDB(ConnPtr);
+            return false;
+        }
+
+        Result = mysql_store_result(ConnPtr);
     }
 
-    unsigned long long index;
-    GetShortURL_Index(index);
-
-    string query = format("INSERT INTO url_info (short_url,long_url)"
-        "SELECT {0}, '{1}' FROM dual WHERE NOT EXISTS"
-        "(SELECT * FROM url_info WHERE `long_url` = '{1}');", index, url);
-
-    Stat = mysql_query(ConnPtr, query.c_str());
-    if (Stat != 0)
+    if (mysql_num_rows(Result) != 1)
     {
-        cout << "Query Error" << mysql_error(Conn) << endl;
-        
-        Disconnect(ConnPtr);
+        DisconnectDB(ConnPtr);
         return false;
     }
-
-
-    string query2 = format("SELECT short_url FROM url_info where long_url = '{}';", url);
-    Stat = mysql_query(ConnPtr, query2.c_str());
-    if (Stat != 0)
-    {
-        cout << "Query Error" << mysql_error(Conn) << endl;
-        //TODO: 처리
-        Disconnect(ConnPtr);
-        return false;
-    }
-    Result = mysql_store_result(ConnPtr);
-
-	if(mysql_num_rows(Result)!=1)
-	{
-        Disconnect(ConnPtr);
-        return false;
-	}
 
     ROW = mysql_fetch_row(Result);
-    cout << ROW[0] << endl;
     mysql_free_result(Result);
 
     if (ROW == NULL)
     {
-        Disconnect(ConnPtr);
+        DisconnectDB(ConnPtr);
         return false;
     }
     string tempURL(ROW[0]);
     shortURL = tempURL;
-    
-    Disconnect(ConnPtr);
+
+    DisconnectDB(ConnPtr);
 
     return true;
+	
 }
 
 bool DBConnector::SearchShortURL_Query(string url, string& longURL)
@@ -154,46 +103,44 @@ bool DBConnector::SearchShortURL_Query(string url, string& longURL)
     MYSQL_RES* Result;
     MYSQL_ROW ROW;
     int Stat;
-
-    ConnPtr = mysql_real_connect(Conn, "localhost", "root", "1234", "mydb", 3306, NULL, 0);
-
-    if (ConnPtr == nullptr)
     {
-        return false;
-    }
+        lock_guard<mutex> guard(dbMutex);
+        ConnPtr = mysql_real_connect(Conn, "localhost", "root", "1234", "mydb", 3306, NULL, 0);
+        if (ConnPtr == nullptr)
+        {
+            return false;
+        }
 
-    string query2 = format("SELECT long_url FROM url_info where short_url = '{}';", url);
-    Stat = mysql_query(ConnPtr, query2.c_str());
-    if (Stat != 0)
-    {
-        cout << "Query Error" << mysql_error(Conn) << endl;
-        //TODO: 처리
-        Disconnect(ConnPtr);
-        return false;
+        string query2 = format("SELECT long_url FROM url_info where short_url = '{}';", url);
+        Stat = mysql_query(ConnPtr, query2.c_str());
+        if (Stat != 0)
+        {
+            cout << "Query Error" << mysql_error(Conn) << endl;
+            DisconnectDB(ConnPtr);
+            return false;
+        }
+        Result = mysql_store_result(ConnPtr);
+
     }
-    Result = mysql_store_result(ConnPtr);
 
     if (mysql_num_rows(Result) != 1)
     {
-        Disconnect(ConnPtr);
+        DisconnectDB(ConnPtr);
         return false;
     }
 
     ROW = mysql_fetch_row(Result);
-    cout << ROW[0] << endl;
     mysql_free_result(Result);
 
     if (ROW == NULL)
     {
-        Disconnect(ConnPtr);
+        DisconnectDB(ConnPtr);
         return false;
     }
 
     string tempURL(ROW[0]);
     longURL = tempURL;
-
-    Disconnect(ConnPtr);
-
+    DisconnectDB(ConnPtr);
     return true;
 }
 
--- URLShortener/URLShortener/DBConnector.h
+++ URLShortener/URLShortener/DBConnector.h
@@ -1,16 +1,9 @@
 #pragma once
 #include <atomic>
+
 class MYSQL;
 
-/**
- * \brief
- * DB URL쇼트너는 DB를 계속 봐야한다.
- * 커넥트를 반복하는 것은 오버헤드가 클 것이다
- * Listener 생성자에서 커넥트를 하고 사용하자
- * 동기화 문제는 접근시 락을 걸까?
- */
 
-void Test();
 class DBConnector
 {
 public:
@@ -19,18 +12,14 @@ public:
 	~DBConnector();
 
 	bool SearchLongURL_Query(string url, string& shortURL);
-
 	bool SearchShortURL_Query(string url, string& longURL);
-
 
 	static void GetShortURL_Index(unsigned __int64& /*OUT*/index);
 
-	//TLS로 범위 지정해줘서 하는 방법도 있지만... 후순위
 	static atomic<unsigned __int64> shortIndex;
-
+	static mutex dbMutex;
 private:
-
-	void Disconnect(MYSQL* ConnPtr);
+	void DisconnectDB(MYSQL* ConnPtr);
 private:
 	
 	MYSQL* Conn=nullptr;                 
